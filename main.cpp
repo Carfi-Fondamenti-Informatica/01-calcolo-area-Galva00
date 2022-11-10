3317f302d857b2abfb287ec502c353337f8191e5
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-11-10 21:05:31 +0100

    Calcolo Funzione
    Galvagno Alessio

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
+    float a=0, b=0, c=0, triangolo=0, quadrato=0, rettangolo=0, trapezio=0;
+    cin >> a >> b >> c;
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    triangolo=(a*b)/2;
+    quadrato=a*a;
+    rettangolo=a*b;
+    trapezio=(a+b)*c/2;
+    cout << triangolo << "\n" << quadrato << "\n" << rettangolo << "\n" << trapezio <<endl;
 }
